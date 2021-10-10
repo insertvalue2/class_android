67dbc7ea07b4e63042b40b1e8f20c6ed393464f6
questzz@naver.com <questzz@naver.com>
2021-10-11 01:07:56 +0900

    viewPager 예제 추가

---
+++ MyViewPager1/app/src/main/java/com/example/myviewpager1/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myviewpager1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+    // 페이지 수
+    private static final int NUM_PAGES = 5;
+    private ViewPager mPager;
+    private PagerAdapter pagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+
+    }
+    // 기본적으로 만들어져 있는 뒤로가기 이벤트 리스너
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // 기본 종료 처리
+            super.onBackPressed();
+        } else {
+            // 4,3,@,1,0
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
+
