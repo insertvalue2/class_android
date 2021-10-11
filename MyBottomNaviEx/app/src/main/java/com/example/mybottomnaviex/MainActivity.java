2ebe3188f2da2e62031f880307bca026d417c68d
questzz@naver.com <questzz@naver.com>
2021-10-11 12:13:30 +0900

    bottomNavigation 예체 추가

---
+++ MyBottomNaviEx/app/src/main/java/com/example/mybottomnaviex/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mybottomnaviex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d("TAG", "itemId : " + item.getItemId());
+                return true;
+            }
+        });
+    }
+}
